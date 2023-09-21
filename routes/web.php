81e06f535dcc3c4f88131cb43646df25ecf2437c
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-09-21 14:00:25 +0300

    Install Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,46 +1,31 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-
-Route::view('/', 'welcome')->name('home');
-
-Route::resource('/posts', PostController::class);
-
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/{id?}', [UserController::class, 'show'])->whereNumber('id')->name('users.show');
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-//?TEST ROUTES
-// Route::get('/', function () {
-//     return view('welcome', ['data' => 'HELLO']);
-// })->name('home');
-
-// Route::get('/test', function () {
-//     return view('test');
-// })->name('test');
-
-// Route::get('/blog', function () {
-//     return view('blog');
-// })->name('blog');
-
-// Route::get('/category/{category}', function(string $category) {
-//     return 'Category: ' . $category;
-// })->whereIn('category', ['movie', 'song', 'painting']);
-
-// Route::get('/users/{id?}', function($id = null) {
-    //     return 'User ' . $id;
-    // })->whereNumber('id')->name('users.show'); // ->where('id', '[0-9]+') the same as whereNumber('id')
-
-// Route::get('/test2', function () {
-//     return 'Test2';
-// });
-
-
+require __DIR__.'/auth.php';

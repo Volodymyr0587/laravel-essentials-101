edd6aad857b280dda082d6360fa20d1f6924e89e
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-09-25 20:42:42 +0300

    use DatabaseTransactions;

--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -3,14 +3,16 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class WelcomeTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
+    use DatabaseTransactions;
     // public function test_the_welcome_page_works(): void
     // {
     //     $response = $this->get('/');

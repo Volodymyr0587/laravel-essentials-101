ec8498f80f7c06ef1c3322c747515b7d7f407a82
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-09-26 15:14:18 +0300

    Test post with database

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,10 +11,27 @@ class PostTest extends TestCase
 {
     use RefreshDatabase;
 
+
     public function test_we_have_no_posts_on_welcome_page(): void
     {
         $response = $this->get('/');
 
         $response->assertSeeText('No Posts Yet');
     }
+
+    public function test_the_post_we_created_exist(): void
+    {
+        $post = Post::factory()->create();
+
+        $response = $this->get('/');
+
+        $this->assertDatabaseCount('posts', 1);
+
+        $this->assertDatabaseHas('posts', [
+            'title' => $post->title,
+        ]);
+
+        $response->assertSeeText($post->title);
+    }
+
 }
